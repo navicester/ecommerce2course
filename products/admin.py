f08319e12a1f0a46d407be2b46678d1a51a0ceb1
navicester <navicester@qq.com>
2016-09-25 17:41:30 +0800

    product variation

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Product
+from .models import Variation
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Variation)
