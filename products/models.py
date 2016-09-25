235c321b4b0c62617059f5f85f61314f9f786acb
navicester <navicester@qq.com>
2016-09-25 17:01:25 +0800

    listview, link-for-model-interface model-manager

--- products/models.py
+++ products/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
+class ProductQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+	def get_queryset(self):
+		return ProductQuerySet(self.model, using=self._db)
+
+	def all(self, *args, **kwargs):
+		return self.get_queryset().active()
 
 # Create your models here.
 class Product(models.Model):
@@ -7,5 +20,11 @@ class Product(models.Model):
 	price = models.DecimalField(decimal_places=2, max_digits=20)
 	active = models.BooleanField(default=True)
 
+	objects = ProductManager()
+	
 	def __unicode__(self): #def __str__(self):
 		return self.title 
+
+	def get_absolute_url(self):
+		return reverse("product_detail", kwargs={"pk": self.pk})
+
--- products/views.py
+++ products/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.http import Http404
+from django.utils import timezone
 
 from .models import Product
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        context["now"] = timezone.now()
+        return context
+
 class ProductDetailView(DetailView):
-	model = Product
-	#template_name = "product.html"
-	#template_name = "<appname>/<modelname>_detail.html"
+    model = Product
+    #template_name = "product.html"
+    #template_name = "<appname>/<modelname>_detail.html"
 
 def product_detail_view_func(request, id):
+    print "coming"
     #product_instance = Product.objects.get(id=id)
-	product_instance = get_object_or_404(Product, id=id)
-	try:
-		product_instance = Product.objects.get(id=id)
-	except Product.DoesNotExist:
-		raise Http404
-	except:
-		raise Http404
+    product_instance = get_object_or_404(Product, id=id)
+    try:
+        product_instance = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
+    except:
+        raise Http404
 
-	template = "products/product_detail.html"
-	context = {	
-		"object": product_instance
-	}
+    template = "products/product_detail.html"
+    context = {    
+        "object": product_instance
+    }
 
-	return render(request, template, context)
+    return render(request, template, context)
