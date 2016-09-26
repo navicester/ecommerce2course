5e098da8341299d6fc79c64a9dc90818ade18c1e
navicester <navicester@qq.com>
2016-09-26 22:01:41 +0800

    search query

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.http import Http404
 from django.utils import timezone
+from django.db.models import Q
 
 from .models import Product
 
@@ -16,6 +17,23 @@ class ProductListView(ListView):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context["now"] = timezone.now()
         return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        if query:
+            qs = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+                )
+            try:
+                qs2 = self.model.objects.filter(
+                    Q(price=query)
+                )
+                qs = (qs | qs2).distinct()
+            except:
+                pass
+        return qs
 
 class ProductDetailView(DetailView):
     model = Product
