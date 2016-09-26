a29e2a6ca3379f36bc17f46e4bce2703976466cc
navicester <navicester@qq.com>
2016-09-27 00:07:06 +0800

    social share, django price

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, VariationListView
 
 urlpatterns = [
     # Examples:
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='products'),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
     #url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_detail_function'),
+    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),    
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.http import Http404
 from django.utils import timezone
 from django.db.models import Q
+from django.contrib import messages
 
-from .models import Product
+from .models import Product, Variation
+from .forms import VariationInventoryFormSet
+from .mixins import StaffRequiredMixin
 
 # Create your views here.
+class VariationListView(StaffRequiredMixin, ListView):
+    model = Variation
+    queryset = Variation.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VariationListView, self).get_context_data(*args, **kwargs)
+        context["formset"] = VariationInventoryFormSet(queryset=self.get_queryset()) 
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        product_pk = self.kwargs.get("pk")
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            queryset = Variation.objects.filter(product=product) 
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        formset = VariationInventoryFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                #if new_item.title:
+                product_pk = self.kwargs.get("pk")
+                product = get_object_or_404(Product, pk=product_pk)
+                new_item.product = product
+                new_item.save()
+                
+            messages.success(request, "Your inventory and pricing has been updated.") 
+            return redirect("products")
+        raise Http404
 
 class ProductListView(ListView):
     model = Product
