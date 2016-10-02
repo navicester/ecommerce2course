de2646d92e5a6436e78249eccc201ffeab9fc0fc
navicester <navicester@qq.com>
2016-10-02 16:32:46 +0800

    related products

--- products/models.py
+++ products/models.py
@@ -16,6 +16,12 @@ class ProductManager(models.Manager):
 	def all(self, *args, **kwargs):
 		return self.get_queryset().active()
 
+	def get_related(self, instance):
+		products_one = self.get_queryset().filter(categories__in=instance.categories.all())
+		products_two = self.get_queryset().filter(default=instance.default)
+		qs = (products_one | products_two).exclude(id=instance.id).distinct()
+		return qs
+
 # Create your models here.
 class Product(models.Model):
 	title = models.CharField(max_length=120)
@@ -26,6 +32,9 @@ class Product(models.Model):
 	default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
 	
 	objects = ProductManager()
+
+	class Meta:
+		ordering = ["-title"]
 
 	def __unicode__(self): #def __str__(self):
 		return self.title 
