3a83f354b9fac88138ca3cc10045714d562aa610
navicester <navicester@qq.com>
2016-10-03 17:37:24 +0800

    product editing with django inline

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,29 @@ from django.contrib import admin
 
 from .models import Product,Variation,ProductImage,Category,ProductFeatured
 
+class ProductImageInline(admin.TabularInline):
+	model = ProductImage
+	extra = 0
+	max_num = 10
+
+class VariationInline(admin.TabularInline):
+	model = Variation
+	extra = 0
+	max_num = 10
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['__unicode__', 'price']
+	inlines = [
+		ProductImageInline,
+		VariationInline,
+	]
+	class Meta:
+		model = Product
+
+admin.site.register(Product, ProductAdmin)
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Variation)
 admin.site.register(ProductImage)
 admin.site.register(Category)
