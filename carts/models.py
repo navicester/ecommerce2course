8a25456bfdb34eb0dcb0e40afcc26dd89cf7e729
navicester <navicester@qq.com>
2016-10-04 14:05:35 +0800

    carts app

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
+from products.models import Variation
+
 # Create your models here.
+class CartItem(models.Model):
+	item = models.ForeignKey(Variation)
+	quantity = models.PositiveIntegerField(default=1)
+
+	def __unicode__(self):
+		return self.item.title
+
+
+class Cart(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+	items = models.ManyToManyField(CartItem)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+	def __unicode__(self):
+		return str(self.id)
--- ecommerce2/settings/base.py
+++ ecommerce2/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 	'newsletter',
     'products',
+    'carts',
     'crispy_forms',
     'registration',
 )
